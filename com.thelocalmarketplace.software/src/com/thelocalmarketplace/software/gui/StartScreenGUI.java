0a148c4110520c8c74d5b40d729be2b89c344777
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-28 08:44:14 -0700

    Added buttons for each functionality on the starting page

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/StartScreenGUI.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/StartScreenGUI.java
@@ -6,17 +6,30 @@ import java.awt.*;
 public class StartScreenGUI {
 	JFrame startScreenFrame;
 	JPanel startScreenPanel;
+	JButton membershipButton;
+	JButton beginCheckoutButton;
+	JButton selectCheckoutStationButton;
+	JButton selectLanguageButton;
 	
 	public StartScreenGUI() {
 		startScreenFrame = new JFrame("TheLocalMarketplace Self-Checkout Station");
 		startScreenPanel = new JPanel();
 		
-		addwidgets();
+		addWidgets();
 		
 		startScreenFrame.getContentPane().add(startScreenPanel, BorderLayout.CENTER);
 		
 		startScreenFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		startScreenFrame.pack();
 		startScreenFrame.setVisible(true);
+	}
+	
+	private void addWidgets() {
+		membershipButton = new JButton("Swipe membership card");
+		beginCheckoutButton = new JButton("Begin checkout");
+		selectCheckoutStationButton = new JButton("Select checkout station type");
+		selectLanguageButton = new JButton("Select language");
+		
+		
 	}
 }
