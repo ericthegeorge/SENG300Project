ba0582a39038f7bfbaf51d0df069aa4d4a110d79
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-28 11:06:41 -0700

    Created addWidgets() method so that the labels appear on the screen

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/CompletionScreenGUI.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/CompletionScreenGUI.java
@@ -24,4 +24,43 @@ public class CompletionScreenGUI {
 		completionScreenFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		completionScreenFrame.setVisible(true);
 	}
+	
+	private void addWidgets() {
+		completionScreenPanel.setLayout(new BoxLayout(completionScreenPanel, BoxLayout.Y_AXIS));
+		
+		// Create labels
+		receiptPrintedLabel = new JLabel("Receipt printed...", SwingConstants.CENTER);
+		collectBagsLabel = new JLabel("Please collect your bags from the bagging area", SwingConstants.CENTER);
+		thankYouMessageLabel = new JLabel("Thank you for shopping with us!");
+		kindMessageLabel = new JLabel("Have a good day!");
+		
+		// Set label alignment
+		receiptPrintedLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		collectBagsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		thankYouMessageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		kindMessageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		// Set label font size
+		Font labelFont = new Font("Arial", Font.PLAIN, 45);
+		receiptPrintedLabel.setFont(labelFont);
+		collectBagsLabel.setFont(labelFont);
+		thankYouMessageLabel.setFont(labelFont);
+		kindMessageLabel.setFont(labelFont);
+		
+		// Add glue to push labels to the center vertically
+		// Add labels and buttons to the main vertical panel
+		completionScreenPanel.add(Box.createVerticalGlue());
+		completionScreenPanel.add(receiptPrintedLabel);
+		completionScreenPanel.add(Box.createVerticalStrut(20));
+		completionScreenPanel.add(collectBagsLabel);
+		completionScreenPanel.add(Box.createVerticalStrut(50));
+		completionScreenPanel.add(thankYouMessageLabel);
+		completionScreenPanel.add(Box.createVerticalStrut(20));
+		completionScreenPanel.add(kindMessageLabel);
+		completionScreenPanel.add(Box.createVerticalGlue());
+	}
+	
+	public static void main(String[] args) {
+		CompletionScreenGUI completionScreen = new CompletionScreenGUI();
+	}
 }
