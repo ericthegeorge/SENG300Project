e5b41e0c4667cadc0e38b124a7d3a4ea5d40f333
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-28 08:32:00 -0700

    Created a package for GUI and added empty StartScreenGUI class

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/StartScreenGUI.java
@@ -0,0 +1,13 @@
+package com.thelocalmarketplace.software.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartScreenGUI {
+	JFrame startScreenFrame;
+	JPanel startScreenPanel;
+	
+	public StartScreenGUI() {
+		
+	}
+}
