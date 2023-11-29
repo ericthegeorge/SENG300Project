c756d2446794c641f12d5b7998fe091e23d86782
Gareth Jenkins <65422175+jenkoman@users.noreply.github.com>
2023-11-28 20:17:09 -0700

    Update KeyboardScreenGUI.java
    Early build

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/KeyboardScreenGUI.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/KeyboardScreenGUI.java
@@ -1,5 +1,38 @@
 package com.thelocalmarketplace.software.gui;
 
-public class KeyboardScreenGUI {
+import javax.swing.*;
+import java.awt.*;
 
+
+public class KeyboardScreenGUI {
+	
+	// Variables for the screen
+	JFrame keyboardScreenFrame;
+	JPanel keyboardScreenPanel;
+	
+	
+	// Constructor for the screen
+	public KeyboardScreenGUI() {
+		JFrame keyboardScreenFrame = new JFrame("The LocalMarketplace Self-Checkout Station");
+		JPanel keyboardScreenPanel = new JPanel();
+		
+		keyboardScreenFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		addWidgets();
+		
+		// TODO: Build out the rest of the screen. 
+		
+	}
+	
+	// Adds all the buttons
+	private void addWidgets() {
+		
+		
+	}
+	
+	// Just for testing screen.
+	public static void main(String[] args) {
+		KeyboardScreenGUI keyboardGUI = new KeyboardScreenGUI();
+	}
 }
+
