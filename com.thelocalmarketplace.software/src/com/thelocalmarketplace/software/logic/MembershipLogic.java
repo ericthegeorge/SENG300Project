183cf7f4b0d09678f4d37c281bb9f48a34adc007
Eric George <erickurian@gmail.com>
2023-11-30 14:31:43 -0700

    created getter for cardData

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/logic/MembershipLogic.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/logic/MembershipLogic.java
@@ -1,4 +1,62 @@
+package com.thelocalmarketplace.software.logic;
 
-public class MembershipLogic extends Abst{
+import com.jjjwelectronics.card.Card;
+import com.jjjwelectronics.card.Card.CardData;
+import com.jjjwelectronics.card.Card.CardSwipeData;
+import com.jjjwelectronics.card.Card.CardTapData;
+import com.thelocalmarketplace.software.AbstractLogicDependant;
 
+public class MembershipLogic extends AbstractLogicDependant{
+	private String number = "";
+	private String cardHolder = "";
+	
+	public String getNumber() {
+		return this.number;
+	}
+	
+	public String getCardHolder() {
+		return this.cardHolder;
+	}
+	
+	
+	public MembershipLogic(CentralStationLogic logic) throws NullPointerException {
+		super(logic);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean enterMembershipByNumber(String number) {
+		//If number is not a valid membership{return false;}
+		//If number is valid, get cardholder name, and set name and number.
+		//What basis is a membership number valid?
+		return false;
+	}
+	
+	public boolean enterMembershipBySwipe(CardSwipeData csd) {
+		//If number is not a valid membership{return false;}
+		//If number is valid, get cardholder name, and set name and number.
+		return false;
+	}
+	
+	public boolean enterMembershipByScan(CardTapData ctd) {
+		
+		//If number is not a valid membership{return false;}
+		//If number is valid, get cardholder name, and set name and number.
+		return false;
+	}
+	
+	private boolean isValidMembership(String number) {
+		
+		return false;
+	}
+	
+	//only get data once or multiplying chance of failure
+	private String[] getCardData(CardData data) {
+		String[] retData = new String[2];
+		retData[0] = data.getNumber();
+		retData[1] = data.getCardholder();
+		return retData;
+	}
+	
+	
+	
 }
