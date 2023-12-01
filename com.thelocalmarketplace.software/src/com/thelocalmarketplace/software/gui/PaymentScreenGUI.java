fd62611a5741ece24064018a35615994a7bc6af1
jennydang61 <140567101+jennydang61@users.noreply.github.com>
2023-11-30 22:33:00 -0700

    Update PaymentScreenGUI.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/PaymentScreenGUI.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/PaymentScreenGUI.java
@@ -5,6 +5,32 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * @author Jenny Dang (30153821)
+ * --------------------------------
+ * @author Adrian Brisebois
+ * @author Alan Yong
+ * @author Ananya jain
+ * @author Andrew Matti
+ * @author Atique Muhammad
+ * @author Christopher Lo
+ * @author Danny Ly
+ * @author Eric George
+ * @author Gareth Jenkins
+ * @author Ian Beler
+ * @author Jahnissi Nwakanma
+ * @author Camila Hernandez 
+ * @author Maheen Nizamani
+ * @author Michael Svoboda
+ * @author Olivia Crosby
+ * @author Rico Manalastas
+ * @author Ryan Korsrud
+ * @author Shanza Raza
+ * @author Sukhnaaz Sidhu
+ * @author Tanmay Mishra
+ * @author Zhenhui Ren
+ */
+
 public class PaymentScreenGUI {
     private JFrame paymentPageFrame;
     private JPanel paymentPagePanel;
