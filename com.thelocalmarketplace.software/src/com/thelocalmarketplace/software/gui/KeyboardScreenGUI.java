254c0c48fb1c712fc971055fbe8c3c4fa3a7e8ea
Gareth Jenkins <65422175+jenkoman@users.noreply.github.com>
2023-11-30 22:03:27 -0700

    Finished Keyboard

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/KeyboardScreenGUI.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/KeyboardScreenGUI.java
@@ -15,7 +15,17 @@ public class KeyboardScreenGUI {
 	JPanel keyRowFourPanel;
 	JPanel keyRowFivePanel;
 	JPanel textAreaPanel;
+	JTabbedPane visualAlphaPanel;
+	JPanel item_A;
+	JPanel item_BtoC;
+	JPanel item_DtoJ;
+	JPanel item_KtoM;
+	JPanel item_NtoP;
+	JPanel item_QtoS;
+	JPanel item_TtoZ;
 	JTextArea textArea;
+	JLabel textBar;
+	String searchText;
 	
 	
 	// Constructor for the screen
@@ -28,9 +38,18 @@ public class KeyboardScreenGUI {
 		keyRowFourPanel = new JPanel();
 		keyRowFivePanel = new JPanel();
 		textAreaPanel = new JPanel();
+		visualAlphaPanel = new JTabbedPane();
+		item_A = new JPanel();
+		item_BtoC = new JPanel();
+		item_DtoJ = new JPanel();
+		item_KtoM = new JPanel();
+		item_NtoP = new JPanel();
+		item_QtoS = new JPanel();
+		item_TtoZ = new JPanel();
+		textBar = new JLabel(searchText);
 		textArea = new JTextArea("Enter Item Name or code");
 		
-		
+		searchText = "ENTER ITEM NAME OR CODE";
 		
 		addWidgets();
 		
@@ -55,60 +74,176 @@ public class KeyboardScreenGUI {
 		keyRowFourPanel.setLayout(new BoxLayout(keyRowFourPanel, BoxLayout.X_AXIS));
 		keyRowFivePanel.setLayout(new BoxLayout(keyRowFivePanel, BoxLayout.X_AXIS));
 		textAreaPanel.setLayout(new BoxLayout(textAreaPanel, BoxLayout.X_AXIS));
-		
+		//visualAlphaPanel.setLayout(new CardLayout());
+		item_A.setLayout(new BoxLayout(item_A, BoxLayout.X_AXIS));
+		item_BtoC.setLayout(new BoxLayout(item_BtoC, BoxLayout.X_AXIS));
+		item_DtoJ.setLayout(new BoxLayout(item_DtoJ, BoxLayout.X_AXIS));
+		item_KtoM.setLayout(new BoxLayout(item_KtoM, BoxLayout.X_AXIS));
+		item_NtoP.setLayout(new BoxLayout(item_NtoP, BoxLayout.X_AXIS));
+		item_QtoS.setLayout(new BoxLayout(item_QtoS, BoxLayout.X_AXIS));
+		item_TtoZ.setLayout(new BoxLayout(item_TtoZ, BoxLayout.X_AXIS));
 		
 		// Add Buttons
+		
+		
+		// Build CardLayout
+		
+		item_A.add(Box.createRigidArea(new Dimension(5,0)));
+		item_A.add(new JButton(" Apples "));
+		item_A.add(Box.createRigidArea(new Dimension(5,0)));
+		item_A.add(new JButton(" Avocado "));
+		item_A.add(Box.createRigidArea(new Dimension(5,0)));
+		item_A.add(new JButton(" Asparagus "));
+		item_A.add(Box.createRigidArea(new Dimension(5,0)));
+		item_BtoC.add(Box.createRigidArea(new Dimension(5,0)));
+		item_BtoC.add(new JButton(" Blueberries "));
+		item_BtoC.add(Box.createRigidArea(new Dimension(5,0)));
+		item_BtoC.add(new JButton(" Beets "));
+		item_BtoC.add(Box.createRigidArea(new Dimension(5,0)));
+		item_BtoC.add(new JButton(" Celery "));
+		item_BtoC.add(Box.createRigidArea(new Dimension(5,0)));
+		item_DtoJ.add(Box.createRigidArea(new Dimension(5,0)));
+		item_DtoJ.add(new JButton(" Endive "));
+		item_DtoJ.add(Box.createRigidArea(new Dimension(5,0)));
+		item_DtoJ.add(new JButton(" Grapes "));
+		item_DtoJ.add(Box.createRigidArea(new Dimension(5,0)));
+		item_DtoJ.add(new JButton(" Jicama "));
+		item_DtoJ.add(Box.createRigidArea(new Dimension(5,0)));
+		item_KtoM.add(Box.createRigidArea(new Dimension(5,0)));
+		item_KtoM.add(new JButton(" Kale "));
+		item_KtoM.add(Box.createRigidArea(new Dimension(5,0)));
+		item_KtoM.add(new JButton(" Lettuce "));
+		item_KtoM.add(Box.createRigidArea(new Dimension(5,0)));
+		item_KtoM.add(new JButton(" Mangos "));
+		item_KtoM.add(Box.createRigidArea(new Dimension(5,0)));
+		item_NtoP.add(Box.createRigidArea(new Dimension(5,0)));
+		item_NtoP.add(new JButton(" Green Peppers "));
+		item_NtoP.add(Box.createRigidArea(new Dimension(5,0)));
+		item_NtoP.add(new JButton(" Onions "));
+		item_NtoP.add(Box.createRigidArea(new Dimension(5,0)));
+		item_NtoP.add(new JButton(" Red Peppers "));
+		item_NtoP.add(Box.createRigidArea(new Dimension(5,0)));
+		item_QtoS.add(Box.createRigidArea(new Dimension(5,0)));
+		item_QtoS.add(new JButton(" Radishes "));
+		item_QtoS.add(Box.createRigidArea(new Dimension(5,0)));
+		item_QtoS.add(new JButton(" Shallots "));
+		item_QtoS.add(Box.createRigidArea(new Dimension(5,0)));
+		item_QtoS.add(new JButton(" Spinach "));
+		item_QtoS.add(Box.createRigidArea(new Dimension(5,0)));
+		item_TtoZ.add(Box.createRigidArea(new Dimension(5,0)));
+		item_TtoZ.add(new JButton(" Tomatoes "));
+		item_TtoZ.add(Box.createRigidArea(new Dimension(5,0)));
+		item_TtoZ.add(new JButton(" Yams "));
+		item_TtoZ.add(Box.createRigidArea(new Dimension(5,0)));
+		item_TtoZ.add(new JButton(" Watermelon "));
+		item_TtoZ.add(Box.createRigidArea(new Dimension(5,0)));
+
+		
+		visualAlphaPanel.addTab("        A        ", item_A);
+		visualAlphaPanel.addTab("      B - C      ", item_BtoC);
+		visualAlphaPanel.addTab("      D - J      ", item_DtoJ);
+		visualAlphaPanel.addTab("      K - M      ", item_KtoM);
+		visualAlphaPanel.addTab("      N - P      ", item_NtoP);
+		visualAlphaPanel.addTab("      Q - S      ", item_QtoS);
+		visualAlphaPanel.addTab("      T - Z      ", item_TtoZ);
+		
+
 		keyRowOnePanel.add(new JButton(" Q "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" W "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" E "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" R "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" T "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" Y "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" U "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" I "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" O "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" P "));
+		keyRowOnePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowOnePanel.add(new JButton(" _ "));
 		
 		keyRowTwoPanel.add(new JButton(" A "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" S "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" D "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" F "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" G "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" H "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" J "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" K "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" L "));
+		keyRowTwoPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowTwoPanel.add(new JButton(" @ "));
 		
 		keyRowThreePanel.add(new JButton(" Z "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" X "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" C "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" V "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" B "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" N "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" M "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" , "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" . "));
+		keyRowThreePanel.add(Box.createRigidArea(new Dimension(5,50)));
 		keyRowThreePanel.add(new JButton(" - "));
 		
+		keyRowFourPanel.add(new JButton(" GO BACK "));
+		keyRowFourPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFourPanel.add(new JButton(" SPACE "));
+		keyRowFourPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFourPanel.add(new JButton(" BACKSPACE "));
+		keyRowFourPanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFourPanel.add(new JButton(" ENTER "));
 		
 		keyRowFivePanel.add(new JButton(" 1 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 2 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 3 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 4 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 5 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 6 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 7 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 8 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 9 "));
+		keyRowFivePanel.add(Box.createRigidArea(new Dimension(5,0)));
 		keyRowFivePanel.add(new JButton(" 0 "));
 		
 		// Add the smaller containers to the main panel
-		textAreaPanel.add(textArea);
+		
+		keyboardScreenPanel.add(visualAlphaPanel, BorderLayout.CENTER);
+		textBar.setText(searchText);
+		textAreaPanel.add(textBar);
+		textAreaPanel.add(Box.createRigidArea(new Dimension(0,50)));
 		keyboardScreenPanel.add(textAreaPanel);
 		keyboardScreenPanel.add(keyRowFivePanel);
 		keyboardScreenPanel.add(keyRowOnePanel);
