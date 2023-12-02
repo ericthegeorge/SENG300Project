1268b054a1b603f403ea1d6a73b65443e9532369
jennydang61 <140567101+jennydang61@users.noreply.github.com>
2023-12-01 19:00:44 -0700

    Create DebitScreenGUI.java

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/DebitScreenGUI.java
@@ -0,0 +1,62 @@
+package com.thelocalmarketplace.software.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DebitScreenGUI {
+    private JFrame debitPageFrame;
+    private JPanel debitPagePanel;
+    
+    public DebitScreenGUI() {
+        debitPageFrame = new JFrame("The LocalMarketplace Self-Checkout Station");
+        debitPagePanel = new JPanel();
+
+        addWidgets();
+
+        debitPageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        debitPageFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        debitPageFrame.setContentPane(debitPagePanel);
+        debitPageFrame.setVisible(true);
+    }
+
+    private void addWidgets() {
+
+        JButton button1 = new JButton("Insert");
+        JButton button2 = new JButton("Tap");
+        JButton button3 = new JButton("Go back");
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	debitPageFrame.dispose();
+            	PaymentScreenGUI paymentScreen = new PaymentScreenGUI();
+            }
+        });
+
+
+        debitPagePanel.setLayout(new GridLayout(1, 3)); 
+        debitPagePanel.add(button1);
+        debitPagePanel.add(button2);
+        debitPagePanel.add(button3);
+    }
+    
+    public static void main(String[] args) {
+        DebitScreenGUI debitScreen = new DebitScreenGUI();
+    }
+}
