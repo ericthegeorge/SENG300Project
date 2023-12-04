c9ed8d9890bc83a0aec8e6c1be0b38990c212409
tanmayMishra17 <119141252+tanmayMishra17@users.noreply.github.com>
2023-12-04 01:08:42 -0700

    Updated PurchaseBagsLogic.java and fixed Bugs

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/logic/PurchaseBagsLogic.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/logic/PurchaseBagsLogic.java
@@ -90,7 +90,7 @@ public class PurchaseBagsLogic extends AbstractLogicDependant{
 		if (!logic.isSessionStarted()) throw new InvalidStateSimulationException("Session has not started");
 		if (!logic.stateLogic.inState(States.ADDBAGS)) throw new InvalidStateSimulationException("Cannot end ADDBAGS state when not in ADDBAGS state");
 		
-		Mass totalBagMass= null;  
+		Mass totalBagMass= new Mass(0);  
 		for (int i = 0;i<numberOfBags;i++ )
 			totalBagMass.sum(bagInstance.getMass());
 		
@@ -98,7 +98,7 @@ public class PurchaseBagsLogic extends AbstractLogicDependant{
 		if (logic.weightLogic.getTotalBagMass().compareTo(totalBagMass) < 0 || this.approvedBagging) {
 			// If bag weight is under the allowed weight
 			this.logic.weightLogic.overrideDiscrepancy();
-			this.approvedBagging = false;
+			this.approvedBagging = true;
 			this.logic.attendantLogic.setBaggingDiscrepency(false);
 			
 			// TODO GUI: 
