feb0393d4fda7bdc1336972257d3ec861b1a78eb
Toto <109338896+goldentoto@users.noreply.github.com>
2023-12-04 13:47:24 -0700

    Update SuspendedListener.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/listeners/SuspendedListener.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/listeners/SuspendedListener.java
@@ -4,18 +4,30 @@ import com.thelocalmarketplace.software.logic.CentralStationLogic;
 
 /**
  * Same as blocked for now
- * @author Connell Reffo (10186960)
- * @author Tara Strickland (10105877)
- * @author Angelina Rochon (30087177)
- * @author Julian Fan (30235289)
- * @author Braden Beler (30084941)
- * @author Samyog Dahal (30194624)
+ * @author Alan Yong (30105707)
+ * @author Andrew Matti (30182547)
+ * @author Olivia Crosby (30099224)
+ * @author Rico Manalastas (30164386)
+ * @author Shanza Raza (30192765)
+ * @author Danny Ly (30127144)
  * @author Maheen Nizmani (30172615)
- * @author Phuong Le (30175125)
- * @author Daniel Yakimenka (10185055)
- * @author Merick Parkinson (30196225)
- * @author Farida Elogueil (30171114)
+ * @author Christopher Lo (30113400)
+ * @author Michael Svoboda (30039040)
+ * @author Sukhnaaz Sidhu (30161587)
+ * @author Ian Beler (30174903)
+ * @author Gareth Jenkins (30102127)
+ * @author Jahnissi Nwakanma (30174827)
+ * @author Camila Hernandez (30134911)
+ * @author Ananya Jain (30196069)
+ * @author Zhenhui Ren (30139966)
+ * @author Eric George (30173268)
+ * @author Jenny Dang (30153821)
+ * @author Tanmay Mishra (30127407)
+ * @author Adrian Brisebois (30170764)
+ * @author Atique Muhammad (30038650)
+ * @author Ryan Korsrud (30173204)
  */
+
 public class SuspendedListener extends BlockedListener {
 
 	public SuspendedListener(CentralStationLogic logic) throws NullPointerException {
