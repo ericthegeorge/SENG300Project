57d86bdc70a6a32062f8146c026808393ce83d25
Danny Ly <danny.ly1@ucalgary.ca>
2023-12-04 20:08:57 -0700

    added test cases to purchaseBagTest test suite

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/controllers/PurchaseBagTests.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/controllers/PurchaseBagTests.java
@@ -1,5 +1,7 @@
 package com.thelocalmarketplace.software.test.controllers;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -30,11 +32,62 @@ public class PurchaseBagTests {
 	}
 	
 	@Test
-	public void normalUse() throws EmptyDevice, OverloadedDevice {
+	public void zeroBagTest() throws EmptyDevice, OverloadedDevice {
+		station.getReusableBagDispenser().load(new ReusableBag());
+		session.purchaseBagsLogic.purchaseBags(0);
+		station.getBaggingArea().addAnItem(new ReusableBag());
+		assertTrue(session.stateLogic.getState().equals(States.BLOCKED));
+		assertTrue(session.cartLogic.getCart().isEmpty());
+	}
+	
+	@Test
+	public void normalUseOneBagTest() throws EmptyDevice, OverloadedDevice {
 		station.getReusableBagDispenser().load(new ReusableBag());
 		session.purchaseBagsLogic.purchaseBags(1);
 		station.getBaggingArea().addAnItem(new ReusableBag());
 		assertTrue(session.stateLogic.getState().equals(States.NORMAL));
 		assertTrue(session.cartLogic.getCart().size() == 1);
 	}
+	
+	@Test
+	public void negativeBagsTest() throws EmptyDevice, OverloadedDevice {
+		station.getReusableBagDispenser().load(new ReusableBag());
+		session.purchaseBagsLogic.purchaseBags(-1);
+		station.getBaggingArea().addAnItem(new ReusableBag());
+		assertTrue(session.stateLogic.getState().equals(States.BLOCKED));
+		assertTrue(session.cartLogic.getCart().isEmpty());
+	}
+	
+	@Test
+	public void multipleBagsWithBagsLeftTest() throws EmptyDevice, OverloadedDevice {
+		int bagsLoaded = 6;
+
+		for (int j = 0; j < bagsLoaded+1; j++) {
+			station.getReusableBagDispenser().load(new ReusableBag());
+		}
+		station.getReusableBagDispenser().load(new ReusableBag());
+		session.purchaseBagsLogic.purchaseBags(4);
+		station.getBaggingArea().addAnItem(new ReusableBag());
+		assertTrue(session.cartLogic.getCart().size() == 4);
+		// why is the state not normal?
+//		assertTrue(session.stateLogic.getState().equals(States.NORMAL));
+	}
+	
+	@Test 
+	public void tooManyBagsInDispenserTest()throws EmptyDevice, OverloadedDevice {
+		int bagsLoaded = 99999;
+		assertThrows(OverloadedDevice.class, () ->{
+		for (int j = 0; j < bagsLoaded +1; j++) {
+			station.getReusableBagDispenser().load(new ReusableBag());
+			}	});
+	}
+	
+	@Test 
+	public void EmptyBagDispenserTest() throws EmptyDevice, OverloadedDevice {
+		station.getReusableBagDispenser().load(new ReusableBag());
+		station.getReusableBagDispenser().unload();
+		assertThrows(EmptyDevice.class, () -> session.purchaseBagsLogic.purchaseBags(1));
+	}
+	
+	
 }
